279497ffb0f44db910533205b35a8fea600106c9
omar <omarbarhoumy229@gmail.com>
2025-02-15 20:34:47 +0100

    Update Navbar.js

--- chess-front/src/components/Navbar.js
+++ chess-front/src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./Navbar.css";
 
 const Navbar = ({ onClose }) => {
@@ -11,67 +12,76 @@ const Navbar = ({ onClose }) => {
   };
 
   return (
-    <nav className="navbar">
-      <div className="logo">ChessClub♔♕♖♗♘♙</div>
-      
+    <nav className="navbar navbar-expand-lg navbar-dark bg-gradient shadow-sm p-3">
+      <div className="container-fluid">
+        <Link className="navbar-brand fw-bold text-uppercase" to="/">
+          ChessClub ♔♕♖♗♘♙
+        </Link>
+        <button
+          className="navbar-toggler border-0"
+          type="button"
+          onClick={toggleNavbar}
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-     
-      <ul className={`nav-list ${isOpen ? "navbar-open" : ""}`}>
-        <li>
-          <Link to="/" className="link" onClick={onClose}>
-          🏠Home
-          </Link>
-        </li>
-
-        {isAuthenticated ? (
-          <>
-            <li>
-              <Link to="/lessons" className="link" onClick={onClose}>
-              📚Learn
+        <div className={`collapse navbar-collapse ${isOpen ? "show" : ""}`}>
+          <ul className="navbar-nav ms-auto text-center">
+            <li className="nav-item">
+              <Link to="/" className="nav-link fw-semibold" onClick={onClose}>
+                🏠 Home
               </Link>
             </li>
-              <li>
-                <Link to="/practice" className="link" onClick={onClose}>
-                🧠 Practice
-                </Link>
-              </li>
-              <li>
-                <Link to="/watch" className="link" onClick={onClose}>
-                📺 watch
-                </Link>
-              </li>
-            <li>
-              <Link to="/memes" className="link" onClick={onClose}>
-              🤪Memes
-              </Link>
-            </li>
-            
-            <li>
-              <Link to="/quiz" className="link" onClick={onClose}>
-              🧩Quiz
-              </Link>
-            </li>
-            <li>
-              <Link to="/logout" className="link" onClick={onClose}>
-              ↪ Logout
-              </Link>
-            </li>
-          </>
-        ) : (
-          <>
-            <li>
-              <Link to="/login" className="link" onClick={onClose}>
-              ↩ Login
-              </Link>
-            </li>
-            <li>
-              <Link to="/signup" className="link" onClick={onClose}>
-              📝 Sign Up
-              </Link>
-            </li>
-          </>
-        )}
-      </ul>
+            {isAuthenticated ? (
+              <>
+                <li className="nav-item">
+                  <Link to="/lessons" className="nav-link fw-semibold" onClick={onClose}>
+                    📚 Learn
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/practice" className="nav-link fw-semibold" onClick={onClose}>
+                    🧠 Practice
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/watch" className="nav-link fw-semibold" onClick={onClose}>
+                    📺 Watch
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/memes" className="nav-link fw-semibold" onClick={onClose}>
+                    🤪 Memes
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/quiz" className="nav-link fw-semibold" onClick={onClose}>
+                    🧩 Quiz
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/logout" className="nav-link text-danger fw-bold" onClick={onClose}>
+                    ↪ Logout
+                  </Link>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className="nav-item">
+                  <Link to="/login" className="nav-link text-success fw-bold" onClick={onClose}>
+                    ↩ Login
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/signup" className="nav-link text-primary fw-bold" onClick={onClose}>
+                    📝 Sign Up
+                  </Link>
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
