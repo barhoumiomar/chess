62f0ad0975d11eee36b2b4d925f250660dc1bbef
omar <omarbarhoumy229@gmail.com>
2025-02-15 16:17:22 +0100

    Update server.js

--- chess-back/server.js
+++ chess-back/server.js
@@ -114,7 +114,7 @@ app.get('/', (req, res) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 5000;  
+const port = process.env.PORT || 5000;  
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${port}`);
 });
